e2c016be75e92d73de749f154084ffc51ab31057
SHAZADA §HARIAR <68187675+shazada-shariar@users.noreply.github.com>
2021-01-01 21:15:23 +0430

    Rename real_host-v2.sh to real-host-v2.sh

---
+++ real-host-v2.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+#Created: 13/11/2020
+#Upgrade 12/01/2021
+#by: real strategy
+#Successful update
+#MESSAGE: Never stop learning, if you are here is out of curiosity and try to learn shell or bash with the help of goo***SULTAN SHARIAR***
